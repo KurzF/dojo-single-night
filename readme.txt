c58d561e973de6dfb38785ce763b193e0ea2e8d1
KurzF <kurzr.f@gmail.com>
2025-10-21 19:09:37 +0200

    solution

---
+++ readme.txt
@@ -0,0 +1,3 @@
+# Dojo single night
+
+Implémentation lors d'un coding dojo du [jour 9 de l'Advent Of Code 2015](https://adventofcode.com/2015/day/9)
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
-use std::collections::{btree_map::Keys, HashMap, HashSet};
+use std::{collections::{HashMap, HashSet}, u32};
 
 fn main() {
     let input = include_str!("../input.txt");
 
+    let map = parse(&input);
+    let result = short_route(&map);
+
+    println!("Shortest distance: {:?}", result);
+}
+
+fn parse(input: &str) -> HashMap<(&str, &str), u32> {
     let mut map = HashMap::new();
 
     for line in input.lines() {
@@ -11,67 +18,84 @@ fn main() {
         split.next();
         let to_city = split.next().unwrap();
         split.next();
-        let value = split.next().unwrap();
+        let value: u32 = split
+            .next().unwrap()
+            .parse().unwrap();
 
-        map.insert((from_city, to_city), value.parse::<u32>());
-        map.insert((to_city, from_city), value.parse::<u32>());
+        map.insert((from_city, to_city), value);
+        map.insert((to_city, from_city), value);
     }
 
+    map
 }
 
-fn short_route(map: HashMap<(&str, &str), u32>) -> u32 {
-    let mut cities = HashSet::new();
+/// Extrait toutes les villes présentent dans la carte
+fn cities<'a>(map: &HashMap<(&'a str, &'a str), u32>) -> HashSet<&'a str> {
+    map.keys().map(|(from, _to)| *from).collect()
+}
+
+fn short_route_from(from: &str, map: &HashMap<(&str, &str), u32>) -> Option<u32> {
+    // Liste des villes a visiter
+    let mut to_visit = cities(&map);
+    to_visit.remove(from);
+
+    // Aucune ville restante
+    if to_visit.is_empty() {
+        return Some(0);
+    }
+
+    // Carte sans la ville actuelle
+    let map_without_current: HashMap<(&str, &str), u32> = map.iter()
+        .filter(|((from_city, to_city), _)| *from_city != from && *to_city != from)
+        .map(|(key, dist)| (*key, *dist))
+        .collect();
+
+    // La plus petite distance est la somme du chemin parcourue entre la première et la deuxième
+    // vile + la plus petite distance entre les autres ville
+    to_visit.iter().flat_map(|city| {
+        map.get(&(from, city))
+            .and_then(|d| short_route_from(city, &map_without_current)
+            .map(|sr| sr + d))
+    }).min()
+}
+
+fn short_route(map: &HashMap<(&str, &str), u32>) -> Option<u32> {
+    let cities = cities(&map);
     
-    for key in map.keys() {
-        cities.insert(key.0);
-    }
-
-    let mut distance_min = u32::MAX;
-
-    for city in cities {
-
-        let map_without_current: HashMap<(&str, &str), u32> = map.iter()
-            .filter(|((from, to), _)| *from != city && *to != city)
-            .map(|(key, dist)| (*key, *dist))
-            .collect();
-        
-        let destination:Vec<&str> = map.iter()
-            .filter(|((from, _), _)| *from == city)
-            .map(|(key, _)| key.1)
-            .collect();
-
-        
-        for d in destination {
-            
-        }
-
-        //for nextCity in cities 
-
-        if !map_without_current.is_empty() {
-            let current_value = short_route(map_without_current);
-
-            if current_value < distance_min {
-                distance_min = current_value;
-            }
-        }
-        
-    }
-
-    return distance_min;
+    cities.iter().flat_map(|city| short_route_from(city, &map)).min()
 }
 
 #[cfg(test)]
 mod test{
-    use std::collections::HashMap;
+    use crate::*;
 
-    use crate::short_route;
+    #[test]
+    fn test_parse() {
+        let input = "Faerun to Tristram = 65";
+        let from = "Faerun";
+        let to = "Tristram";
+
+        let result = parse(input);
+
+        assert_eq!(Some(65), result.get(&(from, to)).copied());
+        assert_eq!(Some(65), result.get(&(to, from)).copied());
+    }
+
+    #[test]
+    fn test_cities() {
+        let input = "a to b = 10";
+        let map = parse(input);
+
+        let result = cities(&map);
+        let expected = ["a", "b"].iter().copied().collect();
+        assert_eq!(result, expected);
+    }
 
     #[test]
     fn test1() {
-        let mut map = HashMap::new();
+        let input = "a to b = 10";
+        let map = parse(input);
 
-        map.insert(("a", "b"), 10);
-
-        assert_eq!(10, short_route(map));
+        assert_eq!(Some(10), short_route(&map));
     }
 }
